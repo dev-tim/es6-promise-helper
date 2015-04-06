e790ff170055ef344b7e93f8ab869cee59eeedd0
azadorozhniy <artem.zadorozhniy@yetu.com>
2015-04-06 23:27:36 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Simple ES6 promise helper for creating Promise object
+in defined state with better syntax.
+
+##Installation
+
+```
+ npm install --save es6-promise-helper
+```
+
+## Questions and contribution
+
+Feel free to make any suggestions, open issue or make PR. 
---
+++ index.js
@@ -0,0 +1,15 @@
+var Promise = require('es6-promise').Promise;
+module.exports = {
+
+  when: function when (value) {
+    return new Promise(function promise (resolve) {
+      resolve(value);
+    });
+  },
+
+  fail: function fail (error) {
+    return new Promise(function promise (resolve, reject) {
+      reject(error);
+    });
+  }
+};
